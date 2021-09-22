9abbaecd0992287e5984214d68e97ca4850fe42a
tarquin-the-brave <tomsteavenson@gmail.com>
2021-09-22 09:31:30 +0100

    update libary with fixes from first solution attempt

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,37 @@
+use anyhow::Result;
 use reqwest::Url;
 use std::collections::{HashMap, HashSet};
-pub fn get_links_html<R: std::io::Read>(html_doc: R) -> anyhow::Result<HashSet<Url>> {
+pub fn get_links_html<R: std::io::Read>(html_doc: R) -> anyhow::Result<HashSet<String>> {
     Ok(
         select::document::Document::from_read(encoding_rs_io::DecodeReaderBytes::new(html_doc))?
             .find(select::predicate::Name("a"))
-            .filter_map(|element| Url::parse(element.attr("href").unwrap()).ok())
-            .collect::<HashSet<Url>>(),
+            .filter_map(|element| element.attr("href").map(|s| s.to_string()))
+            .collect::<HashSet<String>>(),
     )
 }
 
-pub fn output_graph(graph: &HashMap<Url, HashSet<Url>>) {
+///
+/// Ouput URL graph to a writer.
+///
+/// E.g:
+///
+/// ```text
+/// output_graph(&site_graph, std::io::stdout())?;
+/// ```
+pub fn output_graph<W: std::io::Write>(
+    graph: &HashMap<Url, HashSet<Url>>,
+    mut out: W,
+) -> Result<()> {
     for (url, links) in graph {
         if links.is_empty() {
             continue;
         }
-        println!("{} links to:", url);
+        out.write_all(format!("\n{} links to:", url).as_bytes())?;
         for link in links {
-            println!("\t{}", link);
+            out.write_all(format!("\n\t{}", link).as_bytes())?;
         }
     }
+    Ok(())
 }
 
 #[derive(Debug, structopt::StructOpt)]
