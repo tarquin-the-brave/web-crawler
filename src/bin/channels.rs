1be1f11c5947ba80282b38a198ac2b7ca8af987c
tarquin-the-brave <tomsteavenson@gmail.com>
2021-10-02 18:20:19 +0100

    some fixes to channels solution
    Signed-off-by: tarquin-the-brave <tomsteavenson@gmail.com>

--- src/bin/channels.rs
+++ src/bin/channels.rs
@@ -10,17 +10,10 @@ use tokio::{
 use web_crawler::{get_links_html, output_graph, Cli, UrlGraph};
 
 #[derive(Debug)]
-enum Message<T> {
-    Finished,
-    Msg(T),
+enum LinksMsg {
+    Failed(Url),
+    Links(Url, HashSet<Url>),
 }
-
-fn msg<T>(contents: T) -> Message<T> {
-    Message::Msg(contents)
-}
-
-type UrlMsg = Message<Url>;
-type LinksMsg = Message<(Url, HashSet<Url>)>;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -32,11 +25,11 @@ async fn main() -> anyhow::Result<()> {
         .ok_or_else(|| anyhow!("URL given has no host."))?
         .to_owned();
 
-    let (url_tx, url_rx) = channel::<UrlMsg>();
+    let (url_tx, url_rx) = channel::<Url>();
     let (links_tx, links_rx) = channel::<LinksMsg>();
     let (done_tx, mut done_rx) = channel::<UrlGraph>();
 
-    url_tx.send(msg(args.url))?;
+    url_tx.send(args.url)?;
     spawn(fetcher(host, url_rx, links_tx));
     spawn(graph_builder(links_rx, url_tx, done_tx));
 
@@ -46,12 +39,11 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn fetcher(host: String, mut url_rx: Receiver<UrlMsg>, links_tx: Sender<LinksMsg>) {
+async fn fetcher(host: String, mut url_rx: Receiver<Url>, links_tx: Sender<LinksMsg>) {
     loop {
         match url_rx.recv().await {
             None => return,
-            Some(UrlMsg::Finished) => links_tx.send(LinksMsg::Finished).unwrap(),
-            Some(UrlMsg::Msg(url)) => {
+            Some(url) => {
                 spawn(get_links(host.clone(), url, links_tx.clone()));
             }
         };
@@ -60,56 +52,63 @@ async fn fetcher(host: String, mut url_rx: Receiver<UrlMsg>, links_tx: Sender<Li
 
 async fn graph_builder(
     mut links_rx: Receiver<LinksMsg>,
-    url_tx: Sender<UrlMsg>,
+    url_tx: Sender<Url>,
     done_tx: Sender<UrlGraph>,
 ) {
-    let mut maybe_finished = false;
     let mut url_graph = UrlGraph::new();
+    let mut in_flight = HashSet::<Url>::new();
     loop {
         match links_rx.recv().await {
             None => return,
-            Some(LinksMsg::Finished) => {
-                if maybe_finished {
-                    break;
-                } else {
-                    continue;
+            Some(msg) => match msg {
+                LinksMsg::Failed(url) => {
+                    in_flight.remove(&url);
                 }
-            }
-            Some(LinksMsg::Msg((url, links))) => {
-                let mut new_urls = false;
-                for link in &links {
-                    let linkstr = link.as_str();
-                    if linkstr.ends_with(".png")
-                        || linkstr.ends_with(".jpeg")
-                        || linkstr.ends_with(".jpg")
-                        || linkstr.ends_with(".htm")
-                        || linkstr.ends_with(".pdf")
-                    {
-                        continue;
-                    }
-                    let mut link_scrubed = link.clone();
-                    link_scrubed.set_fragment(None);
-                    link_scrubed.set_query(None);
+                LinksMsg::Links(url, links) => {
+                    in_flight.remove(&url);
+                    for link in &links {
+                        let linkstr = link.as_str();
+                        if linkstr.ends_with(".png")
+                            || linkstr.ends_with(".jpeg")
+                            || linkstr.ends_with(".jpg")
+                            || linkstr.ends_with(".htm")
+                            || linkstr.ends_with(".pdf")
+                        {
+                            continue;
+                        }
+                        let mut link_scrubed = link.clone();
+                        link_scrubed.set_fragment(None);
+                        link_scrubed.set_query(None);
 
-                    if link_scrubed != url && !url_graph.contains_key(&link_scrubed) {
-                        new_urls = true;
-                        url_tx.send(msg(link_scrubed)).unwrap();
+                        if link_scrubed != url && !url_graph.contains_key(&link_scrubed) {
+                            url_tx.send(link_scrubed.clone()).unwrap();
+                            in_flight.insert(link_scrubed);
+                        }
                     }
+                    url_graph.insert(url, links);
                 }
-                url_graph.insert(url, links);
+            },
+        }
 
-                if !new_urls {
-                    url_tx.send(UrlMsg::Finished).unwrap();
-                    maybe_finished = true;
-                }
-            }
+        if in_flight.is_empty() {
+            break;
         }
     }
 
     done_tx.send(url_graph).unwrap();
 }
 
-async fn get_links(site_host: String, url: Url, links_tx: Sender<LinksMsg>) -> Result<()> {
+async fn get_links(site_host: String, url: Url, links_tx: Sender<LinksMsg>) {
+    let msg = if let Ok(links) = get_links_inner(&site_host, &url).await {
+        LinksMsg::Links(url, links)
+    } else {
+        LinksMsg::Failed(url)
+    };
+
+    links_tx.send(msg).unwrap()
+}
+
+async fn get_links_inner(site_host: &str, url: &Url) -> Result<HashSet<Url>> {
     reqwest::get(url.clone())
         .await?
         .error_for_status()?
@@ -136,7 +135,6 @@ async fn get_links(site_host: String, url: Url, links_tx: Sender<LinksMsg>) -> R
                         url.join(&link).ok()
                     }
                 })
-                .collect::<HashSet<Url>>()
+                .collect()
         })
-        .and_then(|links| links_tx.send(msg((url, links))).map_err(Into::into))
 }
